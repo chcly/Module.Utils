f44acd56d19b785da5343826bc84b3e14332726a
Charlie C <snailrose@gmail.com>
2023-01-19 11:15:41 -0700

    Update

--- Utils/Timer.cpp
+++ Utils/Timer.cpp
@@ -1,11 +1,7 @@
 /*
 -------------------------------------------------------------------------------
-
     Copyright (c) Charles Carley.
 
-    Contributor(s): none yet.
-
--------------------------------------------------------------------------------
   This software is provided 'as-is', without any express or implied
   warranty. In no event will the authors be held liable for any damages
   arising from the use of this software.
@@ -24,14 +20,14 @@
 -------------------------------------------------------------------------------
 */
 #include "Utils/Timer.h"
-#include <ctime>
 #include "Utils/Definitions.h"
 
-#if RT2_PLATFORM == RT2_PLATFORM_WINDOWS
+#if RT_PLATFORM == RT_PLATFORM_WINDOWS
 #include <Windows.h>
 #else
 #include <sys/time.h>
 #include <unistd.h>
+#include <ctime>
 #endif
 
 namespace Rt2
