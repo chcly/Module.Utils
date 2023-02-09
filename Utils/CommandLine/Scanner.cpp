b5cd8858694db12226a59eca36036c7b03811d56
Charlie C <snailrose@gmail.com>
2023-02-09 12:08:13 -0700

    Update

--- Utils/CommandLine/Scanner.cpp
+++ Utils/CommandLine/Scanner.cpp
@@ -20,7 +20,6 @@
 -------------------------------------------------------------------------------
 */
 #include "Utils/CommandLine/Scanner.h"
-#include "Utils/ParserBase/ScannerBase.h"
 
 namespace Rt2::CommandLine
 {
@@ -68,7 +67,27 @@ namespace Rt2::CommandLine
             --_position;
         tok.setType(TOK_IDENTIFIER);
     }
+    
+    // clang-format off
+#define LowerCaseAz                                                   \
+    'a' : case 'b' : case 'c' : case 'd' : case 'e' : case 'f' : case 'g' \
+        : case 'h' : case 'i' : case 'j' : case 'k' : case 'l' : case 'm' \
+        : case 'n' : case 'o' : case 'p' : case 'q' : case 'r' : case 's' \
+        : case 't' : case 'u' : case 'v' : case 'w' : case 'x' : case 'y' \
+        : case 'z'
 
+#define UpperCaseAz                                                   \
+    'A' : case 'B' : case 'C' : case 'D' : case 'E' : case 'F' : case 'G' \
+        : case 'H' : case 'I' : case 'J' : case 'K' : case 'L' : case 'M' \
+        : case 'N' : case 'O' : case 'P' : case 'Q' : case 'R' : case 'S' \
+        : case 'T' : case 'U' : case 'V' : case 'W' : case 'X' : case 'Y' \
+        : case 'Z'
+
+#define Digits09                                                      \
+    '0' : case '1' : case '2' : case '3' : case '4' : case '5' : case '6' \
+        : case '7' : case '8' : case '9'
+
+    // clang-format on
     void Scanner::lex(Token& tok)
     {
         tok.clear();
