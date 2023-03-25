4387562ecedc2c1261a2d9cb2c1039bea57f4719
Charlie C <snailrose@gmail.com>
2023-03-24 20:36:19 -0600

    Update

---
+++ CMake/Globals.cmake
@@ -0,0 +1,41 @@
+option(Utils_BUILD_TEST          "Build the unit test program." ON)
+option(Utils_AUTO_RUN_TEST       "Automatically run the test program." OFF)
+option(Utils_USE_STATIC_RUNTIME  "Build with the MultiThreaded(Debug) runtime library." ON)
+option(Utils_JUST_MY_CODE        "Enable the /JMC flag" ON)
+option(Utils_OPEN_MP             "Enable low-level fill and copy using OpenMP" ON)
+
+
+set(Utils_EXTRA_FLAGS )
+if (MSVC)
+    # globally disable scoped enum warnings
+    set(Utils_EXTRA_FLAGS "${Utils_EXTRA_FLAGS} /wd26812")
+    
+    
+    set(Utils_EXTRA_FLAGS "${Utils_EXTRA_FLAGS} /W3")
+
+
+    if (Utils_JUST_MY_CODE)
+        # Enable just my code...
+        set(Utils_EXTRA_FLAGS "${Utils_EXTRA_FLAGS} /JMC")
+    endif ()
+
+    set(Utils_EXTRA_FLAGS "${Utils_EXTRA_FLAGS} /fp:precise")
+    set(Utils_EXTRA_FLAGS "${Utils_EXTRA_FLAGS} /fp:except")
+
+    if (Utils_OPEN_MP)
+        add_definitions(-DRT_OPEN_MP=1)
+        set(Utils_EXTRA_FLAGS "${Utils_EXTRA_FLAGS} /openmp")
+    endif()
+
+else ()
+    set(Utils_EXTRA_FLAGS "${Utils_EXTRA_FLAGS} -Os")
+    set(Utils_EXTRA_FLAGS "${Utils_EXTRA_FLAGS} -O3")
+    set(Utils_EXTRA_FLAGS "${Utils_EXTRA_FLAGS} -fPIC")
+
+    if (Utils_OPEN_MP)
+        add_definitions(-DRT_OPEN_MP=1)
+        set(Utils_EXTRA_FLAGS "${Utils_EXTRA_FLAGS} -fopenmp")
+    endif()
+
+    link_libraries(stdc++fs)
+endif ()
